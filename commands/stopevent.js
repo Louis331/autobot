58abcfe054e4a761bc27449f8779e8c4273237b4
Your Name <louiskirkpatrick@googlemail.com>
2020-12-18 14:48:05 +0000

    changed in stop movie

--- commands/stopevent.js
+++ commands/stopevent.js
@@ -11,7 +11,7 @@ module.exports.run = async(bot, msg, msgContent) => {
     if (msg.member.hasPermission('ADMINISTRATOR') &&
         msg.channel.id === currentFile.movieId) {
 
-        var role = msg.guild.roles.cache.find(role => role.name === 'Movie ticket');
+        var role = msg.guild.roles.cache.find(role => role.name === 'Event ticket ticket');
         msg.guild.members.cache.forEach(user => user.roles.remove(role));
         msg.channel.updateOverwrite(msg.guild.id, {
             SEND_MESSAGES: false
